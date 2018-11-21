1759a8aa099352adb9fd82a7cb8ec1bd101501d4
Jaemin Hong <hjm0901@gmail.com>
2018-11-21 18:05:05 +0000

    add HitsSearchers

--- src/main/scala/services/xis/search/searcher/ScriptSearcher.scala
+++ src/main/scala/services/xis/search/searcher/ScriptSearcher.scala
@@ -2,10 +2,34 @@ package services.xis.search.searcher
 
 import java.lang.Integer
 
+class HitsTitleContentSearcher(
+  host: String, port0: Integer, port1: Integer, protocol: String
+) extends TitleContentSearcher(host, port0, port1, protocol) with HitsMixin
+
 class HitsAllFieldsSearcher(
   host: String, port0: Integer, port1: Integer, protocol: String
 ) extends AllFieldsSearcher(host, port0, port1, protocol) with HitsMixin
 
-class _HitsAllFieldsSearcher(
+class HitsSynonymTitleContentSearcher(
   host: String, port0: Integer, port1: Integer, protocol: String
-) extends AllFieldsSearcher(host, port0, port1, protocol) with HitsMixin
+) extends SynonymTitleContentSearcher(host, port0, port1, protocol) with HitsMixin
+
+class HitsSynonymAllFieldsSearcher(
+  host: String, port0: Integer, port1: Integer, protocol: String
+) extends SynonymAllFieldsSearcher(host, port0, port1, protocol) with HitsMixin
+
+class HitsTitleContentConstSearcher(
+  host: String, port0: Integer, port1: Integer, protocol: String
+) extends TitleContentConstSearcher(host, port0, port1, protocol) with HitsMixin
+
+class HitsAllFieldsConstSearcher(
+  host: String, port0: Integer, port1: Integer, protocol: String
+) extends AllFieldsConstSearcher(host, port0, port1, protocol) with HitsMixin
+
+class HitsSynonymTitleContentConstSearcher(
+  host: String, port0: Integer, port1: Integer, protocol: String
+) extends SynonymTitleContentConstSearcher(host, port0, port1, protocol) with HitsMixin
+
+class HitsSynonymAllFieldsConstSearcher(
+  host: String, port0: Integer, port1: Integer, protocol: String
+) extends SynonymAllFieldsConstSearcher(host, port0, port1, protocol) with HitsMixin
