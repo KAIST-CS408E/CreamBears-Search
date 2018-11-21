c320bcf05faf8db1377a57d7d8ad4cf3c7fb6af8
Jaemin Hong <hjm0901@gmail.com>
2018-11-21 17:30:57 +0000

    add TitleContentSearcher

--- src/main/scala/services/xis/search/searcher/CombinedSearchers.scala
+++ src/main/scala/services/xis/search/searcher/CombinedSearchers.scala
@@ -10,7 +10,7 @@ abstract class CombinedSearcher(
   host: String, port0: Integer, port1: Integer, protocol: String,
   components: List[(GenBuilderSearcher, Float)]
 ) extends GenBuilderSearcher(host, port0, port1, protocol) {
-  def builder(key: String): BoolQueryBuilder =
+  def builder(key: String): QueryBuilder =
     (new BoolQueryBuilder() /: components){
       case (builder, (searcher, boost)) =>
         builder.should(searcher.builder(key).boost(boost))
@@ -22,6 +22,14 @@ abstract class CombinedSearcher(
   }
 }
 
+class TitleContentSearcher(
+  host: String, port0: Integer, port1: Integer, protocol: String,
+) extends CombinedSearcher(
+  host, port0, port1, protocol,
+  List((new BaseTitleSearcher(host, port0, port1, protocol), 2f),
+       (new BaseContentSearcher(host, port0, port1, protocol), 1f))
+)
+
 class AllFieldsSearcher(
   host: String, port0: Integer, port1: Integer, protocol: String,
 ) extends CombinedSearcher(
@@ -30,6 +38,14 @@ class AllFieldsSearcher(
        (new BaseContentSearcher(host, port0, port1, protocol), 1f),
        (new BaseImageSearcher(host, port0, port1, protocol), .5f),
        (new BaseAttachedSearcher(host, port0, port1, protocol), .2f))
+)
+
+class SynonymTitleContentSearcher(
+  host: String, port0: Integer, port1: Integer, protocol: String,
+) extends CombinedSearcher(
+  host, port0, port1, protocol,
+  List((new SynonymTitleSearcher(host, port0, port1, protocol), 2f),
+       (new SynonymContentSearcher(host, port0, port1, protocol), 1f))
 )
 
 class SynonymAllFieldsSearcher(
